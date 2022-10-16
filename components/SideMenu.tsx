e1ab9b561d8eec48349493fa11f9dbafb3100b28
Collinson <65322305+Collinson165@users.noreply.github.com>
2022-10-16 05:29:23 +0100

    Update SideMenu.tsx

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -8,12 +8,12 @@ const Menu = (props) => {
             <div className="text-3xl flex justify-end" onClick={() => props.menu(false)}><XMarkIcon className="h-10" /></div>
 
             <div className="px-5">
-                <Link href="/"><div className="flex items-center py-5"><img src="/images/Home.svg" alt="" /> Home</div></Link>
-                <Link href="/Collections"><div className="flex items-center py-5"><img src="/images/playlist.svg" alt=""  className="pl-2 mr-3"/> My Collections</div></Link>
-                <Link href="/"><div className="flex items-center py-5"><img src="/images/radio.svg" alt=""  className="pl-2 mr-3"/>Radio</div></Link>
-                <Link href="/"><div className="flex items-center py-5"><img src="/images/videos.svg" alt="" className="pl-2 mr-3" />Music Videos</div></Link>
-                <Link href="/"><div className="flex items-center py-5"><img src="/images/profile.svg" alt="" className="pl-2 mr-3" />Profile</div></Link>
-                <Link href="/"><div className="flex items-center py-5"><img src="/images/Logout.svg" alt="" className="pl-2 mr-3" />Logout</div></Link>
+                <Link href="/"><div className="flex items-center py-5 cursor-pointer"  onClick={() => props.menu(false)}><img src="/images/Home.svg" alt="" /> Home</div></Link>
+                <Link href="/Collections"><div className="flex items-center py-5 cursor-pointer"  onClick={() => props.menu(false)}><img src="/images/playlist.svg" alt=""  className="pl-2 mr-3"/> My Collections</div></Link>
+                <Link href="/"><div className="flex items-center py-5 cursor-pointer"  onClick={() => props.menu(false)}><img src="/images/radio.svg" alt=""  className="pl-2 mr-3"/>Radio</div></Link>
+                <Link href="/"><div className="flex items-center py-5 cursor-pointer"  onClick={() => props.menu(false)}><img src="/images/videos.svg" alt="" className="pl-2 mr-3" />Music Videos</div></Link>
+                <Link href="/"><div className="flex items-center py-5 cursor-pointer"  onClick={() => props.menu(false)}><img src="/images/profile.svg" alt="" className="pl-2 mr-3" />Profile</div></Link>
+                <Link href="/"><div className="flex items-center py-5 cursor-pointer"  onClick={() => props.menu(false)}><img src="/images/Logout.svg" alt="" className="pl-2 mr-3" />Logout</div></Link>
                 
                 
             </div>
