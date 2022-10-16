25412fee3c591aa2b3f94c32c298e799e5413fb2
Collinson <65322305+Collinson165@users.noreply.github.com>
2022-10-16 05:26:08 +0100

    fixed collections link

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -9,7 +9,7 @@ const Menu = (props) => {
 
             <div className="px-5">
                 <Link href="/"><div className="flex items-center py-5"><img src="/images/Home.svg" alt="" /> Home</div></Link>
-                <Link href="/"><div className="flex items-center py-5"><img src="/images/playlist.svg" alt=""  className="pl-2 mr-3"/> My Collections</div></Link>
+                <Link href="/Collections"><div className="flex items-center py-5"><img src="/images/playlist.svg" alt=""  className="pl-2 mr-3"/> My Collections</div></Link>
                 <Link href="/"><div className="flex items-center py-5"><img src="/images/radio.svg" alt=""  className="pl-2 mr-3"/>Radio</div></Link>
                 <Link href="/"><div className="flex items-center py-5"><img src="/images/videos.svg" alt="" className="pl-2 mr-3" />Music Videos</div></Link>
                 <Link href="/"><div className="flex items-center py-5"><img src="/images/profile.svg" alt="" className="pl-2 mr-3" />Profile</div></Link>
